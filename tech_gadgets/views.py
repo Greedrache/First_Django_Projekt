5fb43093faa80666986161e1db478ba37842d57a
Greedrache <greedrache@gmail.com>
2026-02-09 17:57:30 +0100

    Django Grundlagen

---
+++ tech_gadgets/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+
+from .dummy_data import gadgets
+
+# Create your views here.
+
+def start_page_view(request):
+    return HttpResponse("Hello World")
+
+def single_gadget_view(request):
+    return HttpResponse(gadgets[0])
