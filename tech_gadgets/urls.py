22a0c5bfda37d19830a396d292c40a9189518a99
Greedrache <greedrache@gmail.com>
2026-02-10 16:16:30 +0100

    #

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import start_page_view, single_gadget_view
+from .views import single_slug_view, start_page_view, single_gadget_view
 
 urlpatterns = [
     path('', start_page_view),
-    path('gadget/', single_gadget_view)
+    path('gadget/<int:gadget_id>', single_gadget_view),
+    path('gadget/<slug:gadget_slug>', single_slug_view, name="gadget_slug_url")
+      #integer wichtig damit Programm weiß, dass es sich um eine Zahl handelt
+      #string wandelt Buchstaben in string um, aber auch Zahlen in string, deswegen int
 ]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import  HttpResponse
+from django.shortcuts import redirect #render
+from django.http import  HttpResponse, JsonResponse
+from json  import dumps
+from django.utils.text import slugify
+from django.urls import reverse
 
 from .dummy_data import gadgets
 
@@ -8,5 +11,16 @@ from .dummy_data import gadgets
 def start_page_view(request):
     return HttpResponse("Hello World")
 
-def single_gadget_view(request):
-    return HttpResponse(gadgets[0])
+def single_gadget_view(request, gadget_id):
+
+    new_slug = slugify(gadgets[gadget_id]["name"])
+    new_url = reverse("gadget_slug_url", args=[new_slug])
+    return redirect(new_url) #slugs bindestrich statt leerzeichen
+
+def single_slug_view(request, gadget_slug):
+    gadget_match = {"result": "nothing found"}
+
+    for gadget in gadgets:
+        if slugify(gadget["name"]) == gadget_slug:
+            gadget_match = gadget
+    return JsonResponse(gadget_match) #slugs bindestrich statt leerzeichen
